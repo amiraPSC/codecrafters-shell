a287f61efbdb52de972f9c15ae4436b91f698b27
Amira Alshaar <amira2psc@gmail.com>
2026-05-18 17:56:33 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,12 @@ public class Main {
             String input = sc.nextLine();
             if (input.equals("exit")){
                 break;
+            }else if (input.startsWith("echo ")){
+                String[] split = input.split(" ");
+                for (int i = 1; i < split.length; i++){
+                    System.out.print(split[i]);
+                }
+                System.out.println();
             }
             System.out.println(input + ": command not found");
         }
