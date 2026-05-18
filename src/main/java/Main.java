4ea32e47093039813295b9a3cf50896af484e5e6
Amira Alshaar <amira2psc@gmail.com>
2026-05-18 17:35:07 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.print(input + ": command not found");
     }
 }
