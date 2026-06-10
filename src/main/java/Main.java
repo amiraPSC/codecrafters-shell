0756342d4647a748c7bbf6144abc0eb97129f8c5
Amira Alshaar <amira2psc@gmail.com>
2026-06-10 17:04:11 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,29 @@
 import commands.Command;
 import commands.CommandFactory;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 import parser.CommandLine;
+import parser.ShellCompleter;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        try (Terminal terminal = TerminalBuilder.builder().build()) {
+            LineReader reader = LineReaderBuilder.builder()
+                    .terminal(terminal)
+                    .completer(new ShellCompleter())
+                    .build();
 
-        while (true){
-            System.out.print("$ ");
-            String input = sc.nextLine();
+            while (true) {
+                String line = reader.readLine("& ");
 
-            CommandLine commandLine = new CommandLine(input);
-            Command cmd = CommandFactory.getCommand(commandLine.getCommand());
-            cmd.execute(commandLine);
+                CommandLine commandLine = new CommandLine(line);
+                Command cmd = CommandFactory.getCommand(commandLine.getCommand());
+                cmd.execute(commandLine);
+            }
         }
     }
 }
---
+++ src/main/java/parser/ShellCompleter.java
@@ -0,0 +1,21 @@
+package parser;
+
+import org.jline.reader.Candidate;
+import org.jline.reader.Completer;
+import org.jline.reader.LineReader;
+import org.jline.reader.ParsedLine;
+
+import java.util.List;
+
+public class ShellCompleter implements Completer {
+    private static final List<String> commands = List.of("echo", "exit");
+    @Override
+    public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
+        String word = line.word();
+        for (String command : commands) {
+            if (command.startsWith(word)) {
+                candidates.add(new Candidate(command + " "));
+            }
+        }
+    }
+}
