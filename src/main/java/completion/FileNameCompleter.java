dcef9325c0d3f38e19b1287103d190d06cb03b9b
Amira Alshaar <amira2psc@gmail.com>
2026-06-29 17:22:21 +0300

    codecrafters submit [skip ci]

--- src/main/java/completion/FileNameCompleter.java
+++ src/main/java/completion/FileNameCompleter.java
@@ -13,22 +13,24 @@ public class FileNameCompleter implements Completer {
     @Override
     public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
         String word = line.word();
-        Path dir = null;
+        String prefix;
+        Path dir;
 
         if (word.contains("/")){
             int lastSlash = word.lastIndexOf("/");
             String name = word.substring(0, lastSlash + 1);
             String dirName = (PathScanning.getCurrentDir().getFileName().toString()) + "/" + name;
-            word = word.substring(lastSlash + 1);
+            prefix = word.substring(lastSlash + 1);
 
 
             dir = Path.of(dirName);
         }else {
+            prefix = word;
             dir = PathScanning.getCurrentDir();
         }
 
         for (String file : PathScanning.getFilesInCurrentDirectory(dir)) {
-            if (file.startsWith(word)) {
+            if (file.startsWith(prefix)) {
                 candidates.add(new Candidate(file));
             }
         }
