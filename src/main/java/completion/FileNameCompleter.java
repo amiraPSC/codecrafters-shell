4be120eedf9a8c7d1c6b628362ac5dbaee02fcc0
Amira Alshaar <amira2psc@gmail.com>
2026-07-02 16:34:26 +0300

    codecrafters submit [skip ci]

--- src/main/java/completion/FileNameCompleter.java
+++ src/main/java/completion/FileNameCompleter.java
@@ -14,6 +14,7 @@ public class FileNameCompleter implements Completer {
     public void complete(LineReader reader, ParsedLine line, List<Candidate> candidates) {
         String buffer = line.line();
         String word = line.word();
+        System.out.println(word);
         Path dir = getDir(word);
         String prefix = getPrefix(word);
 
