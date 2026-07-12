c9e9b92c1ffd04ba33e8e8242c38b4b45c6adf90
Amira Alshaar <amira2psc@gmail.com>
2026-07-12 16:42:06 +0300

    codecrafters submit [skip ci]

--- src/main/java/commands/impl/CompleteCommand.java
+++ src/main/java/commands/impl/CompleteCommand.java
@@ -7,22 +7,41 @@ import java.util.List;
 import java.util.Map;
 
 public class CompleteCommand implements Command {
-    private static Map<String, List<String>> map = new HashMap<>();
+    private static Map<String, String> map = new HashMap<>();
 
     @Override
     public void execute(CommandLine commandLine) throws Exception {
         List<String> args = commandLine.getArgs();
-        if (args.get(0).equals("-p")){
-            print(args.get(1));
+        String command = args.get(0);
+
+        switch (command) {
+            case "-p":
+                print(args.get(1));
+                break;
+            case "-C":
+                registerCompletionScript(commandLine);
+                break;
         }
+    }
+
+    private void registerCompletionScript(CommandLine commandLine) {
+        List<String> args = commandLine.getArgs();
+        String command = commandLine.getCommand();
+        StringBuilder script = new StringBuilder();
+
+        script.append("\'");
+        for (String arg : args) {
+            script.append(arg).append(" ");
+        }
+        script.deleteCharAt(script.length() - 1);
+        script.append("\'");
+
+        map.put(command, script.toString());
     }
 
     private void print(String command) {
         if (map.containsKey(command)) {
-            List<String> list = map.get(command);
-            for (String s : list) {
-                System.out.println(s);
-            }
+            System.out.println(String.format("complete -C %1$s %2$s", map.get(command), command));
         }else {
             System.out.println(String.format("complete: %s: no completion specification", command));
         }
