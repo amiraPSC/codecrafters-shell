3e0f4a24b4fc65b507391ce79517c4c5e958d5e3
Amira Alshaar <amira2psc@gmail.com>
2026-07-12 17:14:26 +0300

    codecrafters submit [skip ci]

--- src/main/java/parser/Reader.java
+++ src/main/java/parser/Reader.java
@@ -6,18 +6,14 @@ import org.jline.reader.LineReaderBuilder;
 import org.jline.terminal.Terminal;
 
 public class Reader {
-    private static final LineReader reader;
-    private static Terminal terminal;
-    static {
-        reader = LineReaderBuilder.builder()
+    private final LineReader reader;
+
+    public Reader(Terminal terminal) {
+        this.reader = LineReaderBuilder.builder()
                 .terminal(terminal)
                 .completer(CompleterFactory.create())
                 .option(LineReader.Option.DISABLE_EVENT_EXPANSION, true)
                 .build();
-    }
-
-    public Reader(Terminal terminal) {
-        this.terminal = terminal;
     }
 
     public LineReader getLineReader(){
