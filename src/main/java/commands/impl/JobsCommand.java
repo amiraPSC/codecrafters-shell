fd93ee0409f10e7437b9a38eeed457b53cc4b86d
Amira Alshaar <amira2psc@gmail.com>
2026-07-21 19:21:25 +0300

    codecrafters submit [skip ci]

--- src/main/java/commands/impl/JobsCommand.java
+++ src/main/java/commands/impl/JobsCommand.java
@@ -10,6 +10,8 @@ import java.util.List;
 public class JobsCommand implements Command {
     @Override
     public void execute(Parser parser) throws Exception {
+        if (!parser.hasBackgroundOperator()) return;
+        
         List<String> lineCommand = parser.getArgsWithCommand();
         JobManager jobManager = new JobManager();
         Job job = jobManager.addJob(lineCommand);
