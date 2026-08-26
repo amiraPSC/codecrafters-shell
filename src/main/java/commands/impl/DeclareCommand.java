c6e3a55e9de07319b45da234025322cc753de2b3
Amira Alshaar <amira2psc@gmail.com>
2026-08-26 18:39:13 +0300

    codecrafters submit [skip ci]

--- src/main/java/commands/impl/DeclareCommand.java
+++ src/main/java/commands/impl/DeclareCommand.java
@@ -6,11 +6,12 @@ import parser.nodes.impl.CommandNode;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DeclareCommand implements Command {
-    private static Map<String, String> parameters;
+    private static Map<String, String> parameters = new HashMap<>();
 
     @Override
     public void execute(CommandNode node, ExecutionContext context) throws IOException {
