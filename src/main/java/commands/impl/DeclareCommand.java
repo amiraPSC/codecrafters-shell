1a186ba06a4620f9baf53901cd38efcdf9ee685f
Amira Alshaar <amira2psc@gmail.com>
2026-08-26 18:27:59 +0300

    codecrafters submit [skip ci]

--- src/main/java/commands/impl/DeclareCommand.java
+++ src/main/java/commands/impl/DeclareCommand.java
@@ -5,10 +5,28 @@ import executors.ExecutionContext;
 import parser.nodes.impl.CommandNode;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
 
 public class DeclareCommand implements Command {
+    private Map<String, String> parameters;
 
     @Override
     public void execute(CommandNode node, ExecutionContext context) throws IOException {
+        String option = node.getArgs().getFirst();
+
+        switch (option) {
+            case "-p":
+        }
+    }
+
+    private void printDescriptionVariable(String variable, ExecutionContext context) {
+        PrintWriter out = context.getWriter();
+
+        if (parameters.containsKey(variable)) {
+            out.println(variable + " " + parameters.get(variable));
+        }else {
+            out.printf("declare: %s: not found", variable);
+        }
     }
 }
